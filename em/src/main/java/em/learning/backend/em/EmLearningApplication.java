94c3badbffe687d120307a694404c28515133ecd
- Edwin Mendez - <emendez@unac.edu.co>
2018-11-12 19:39:49 -0500

    eureka

---
+++ em/src/main/java/em/learning/backend/em/EmLearningApplication.java
@@ -0,0 +1,12 @@
+package em.learning.backend.em;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmLearningApplication.class, args);
+	}
+}
---
+++ em/src/test/java/em/learning/backend/em/EmLearningApplicationTests.java
@@ -0,0 +1,16 @@
+package em.learning.backend.em;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmLearningApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
